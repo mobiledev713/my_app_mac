31f022f3fa948ad6101b0ec3e90f5588ef336d49
mobiledev713 <vasyl.hutsko713@outlook.com>
2021-07-22 19:46:13 -0400

    Initial Commit

---
+++ My App/My_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AppApp.swift
+//  My App
+//
+//  Created by admin on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct My_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
